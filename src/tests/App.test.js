e9cbb8d26b71145daf64be4c437dddf5a7d3cd2c
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-03-22 15:20:01 -0300

    Requisito um concluído

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,54 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('1 - Teste se o topo da aplicação contém um conjunto fixo de links de navegação',
+  () => {
+    it('Testa o redirecionamento para a rota "/" ao clicar no link "Home"',
+      () => {
+        const { history } = renderWithRouter(<App />);
+        const homeLink = screen.getByRole('link', { name: 'Home' });
+        expect(homeLink).toBeDefined();
+
+        userEvent.click(homeLink);
+        const { location: { pathname } } = history;
+        expect(pathname).toBe('/');
+      });
+
+    it('Testa o redirecionamento para a rota "/about" ao clicar no link "About"',
+      () => {
+        const { history } = renderWithRouter(<App />);
+        const aboutLink = screen.getByRole('link', { name: 'About' });
+        expect(aboutLink).toBeDefined();
+
+        userEvent.click(aboutLink);
+        const { location: { pathname } } = history;
+        expect(pathname).toBe('/about');
+      });
+
+    it('Testa o redirecionamento para a rota "/favorites" ao clicar no link "Favorite"',
+      () => {
+        const { history } = renderWithRouter(<App />);
+        const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+        expect(favoriteLink).toBeDefined();
+
+        userEvent.click(favoriteLink);
+        const { location: { pathname } } = history;
+        expect(pathname).toBe('/favorites');
+      });
+
+    it('Testa o redirecionamento para a página "Not Found" em uma rota desconhecida',
+      () => {
+        const { history } = renderWithRouter(<App />);
+
+        history.push('/pagina-aleatoria');
+        const title = screen.getByRole('heading', {
+          name: 'Page requested not found Crying emoji',
+          level: 2,
+        });
+
+        expect(title).toBeDefined();
+      });
+  });
